0ffc9685c27921e2ed5110bac3df0ad302f4782e
simonasstaskevicius <simonas.staskevicius@hostinger.com>
2025-05-27 21:08:58 +0300

    fix

--- clientControllers/menuController.js
+++ clientControllers/menuController.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const getMenuByRestaurant = async (req, res) => {
+const getMenuByRestaurants = async (req, res) => {
     const { id } = 
 }
